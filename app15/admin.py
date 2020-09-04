81d9907ca7637c4fa3d0a1fe913364f88e728912
roopendrar <roopendrark@gmail.com>
2020-09-04 19:40:37 +0530

    second commit

--- app15/admin.py
+++ app15/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from app15 import models
 
 # Register your models here.
+class TopicAdminView(admin.ModelAdmin):
+    list_display=('topic_name',)
+    search_fields=('topic_name',)
+
+class WebpageAdminView(admin.ModelAdmin):
+    list_display=('topic','name','url')
+    search_fields=('topic','name')
+    list_editable=('name',)
+    list_filter=('topic',)
+
+class AccessDetailsAdminView(admin.ModelAdmin):
+    list_display=('Webpage','datetime')
+    search_fields=('Webpage','datetime')
+
+
+admin.site.register(models.Topic,TopicAdminView)
+admin.site.register(models.Webpage,WebpageAdminView)
+admin.site.register(models.AccessDetails,AccessDetailsAdminView)
